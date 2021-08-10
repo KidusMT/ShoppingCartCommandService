2debe6ac2acacbfe1c4f8710235a0772f4dfe136
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-08-10 00:55:04 -0500

    first commit

---
+++ src/main/java/project/swa/ShoppingCartService/ShoppingCartServiceApplication.java
@@ -0,0 +1,13 @@
+package project.swa.ShoppingCartService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/swa/ShoppingCartService/ShoppingCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package project.swa.ShoppingCartService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
